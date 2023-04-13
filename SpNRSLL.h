fd717a2fb8ee0c3a8664f1cad744d12d0e765882
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-04-13 12:41:15 +0700

    ubah header

--- SpNRSLL.h
+++ SpNRSLL.h
@@ -4,8 +4,8 @@
 #include <stdio.h>
 #define Nil NULL
 #define Info(P) (P)->info
-#define NextGroup(P) (P)->nextgrup
-#define NextTim(P) (P)->nexttim
+#define nextGroup(P) (P)->nextGroup
+#define nextGroupToTeam(P) (P)->nextGroupToTeam
 //#define Info(Q) (Q)->info
 #define Skor(Q) (Q)->skor
 #define Gol(Q) (Q)->gol
@@ -15,57 +15,49 @@
 //#define Pertama(L) (L).Pertama
 
 typedef char *infotype;
-typedef struct tElmtList *address;
-typedef struct tdatatim *addressTim;
-typedef struct tElmtList {
-	infotype info;
-	address nextgrup;
-	addressTim nexttim;
-}ElmtList;
+typedef struct tDataGroup *addressGroup;
+typedef struct tDataTeam *addressTeam;
+typedef struct tDataGroup {
+	infotype infoGroup;
+	addressGroup nextGroup;
+	addressTeam nextGroupToTeam;
+}DataGroup;
 
-typedef struct tdatatim {
-	infotype info;
+typedef struct tDataTeam {
+	infotype infoTeam;
 	int skor;
 	int gol;
 	int selisih;
-	addressTim next;
-}dataTim;
+	addressTeam next;
+}DataTeam;
 
 typedef struct {
-	address First;
+	addressGroup First;
 } ListGroup;
 
 typedef struct {
-	addressTim First;
-} ListTim;
+	addressTeam First;
+} ListTeam;
 
 void CreateListGroup (ListGroup * L);
 /* IS : L sembarang */
 /* FS : Terbentuk ListGroup Kosong */
 
-void CreateListTim (ListTim * L);
+void CreateListTeam (ListTeam * L);
 /* IS : L sembarang */
-/* FS : Terbentuk ListTim Kosong */
+/* FS : Terbentuk ListTeam Kosong */
 
-address AlokasiGroup (infotype X);
+addressGroup AlokasiGroup (infotype X);
 /* Mengirimkan addressGroup hasil alokasiGroup sebuah elemen */
 /* Jika alokasiGroup berhasil, maka addressGroup != Nil, 	   */
-/*	dan misalnya menghasilkan P, maka Info(P) = X, NextGroup(P) = Nil, NextTim(P) = Nil */
+/*	dan misalnya menghasilkan P, maka Info(P) = X, nextGroup(P) = Nil, nextGroupToTeam(P) = Nil */
 /* Jika alokasiGroup gagal, mengirimkan Nil */
 
-addressTim AlokasiTim (infotype X);
-/* Mengirimkan addressTim hasil alokasiTim sebuah elemen */
-/* Jika alokasiTim berhasil, maka addressTim != Nil, 	   */
+addressTeam AlokasiTeam (infotype X);
+/* Mengirimkan addressTeam hasil AlokasiTeam sebuah elemen */
+/* Jika AlokasiTeam berhasil, maka addressTeam != Nil, 	   */
 /*	dan misalnya menghasilkan P, maka Info(P) = X, Next(P) = Nil */
-/* Jika alokasiTim gagal, mengirimkan Nil */
-
-void InsertFirstGroup (ListGroup * L, address P);
-/* IS : L sembarang, P sudah dialokasi */
-/* FS : menambahkan elemen ber-addressGroup P sebagai elemen pertama */
-
-void InsertFirstTim (ListTim * L, addressTim P);
-/* IS : L sembarang, P sudah dialokasi */
-/* FS : menambahkan elemen ber-addressGroup P sebagai elemen pertama */
+/* Jika AlokasiTeam gagal, mengirimkan Nil */
 
 void InsVLastGroup (ListGroup * L, infotype X);
 /* IS : L mungkin Kosong */
@@ -73,26 +65,26 @@ void InsVLastGroup (ListGroup * L, infotype X);
 /* menambahkan elemen listGroup di akhir (elemen terakhir adalah yang baru) */
 /* bernilai X jika alokasiGroup berhasil. Jika alokasiGroup gagal IS = FS */
 
-void InsVLastTim (ListTim * L, infotype X);
+void InsVLastTeam (ListTeam * L, infotype X);
 /* IS : L mungkin Kosong */
-/* FS : melakukan alokasiTim sebuah elemen dan */
-/* menambahkan elemen listTim di akhir (elemen terakhir adalah yang baru) */
-/* bernilai X jika alokasiTim berhasil. Jika alokasiTim gagal IS = FS */
+/* FS : melakukan AlokasiTeam sebuah elemen dan */
+/* menambahkan elemen ListTeam di akhir (elemen terakhir adalah yang baru) */
+/* bernilai X jika AlokasiTeam berhasil. Jika AlokasiTeam gagal IS = FS */
 
 void PrintInfo (ListGroup L);
 /* IS : L mungkin kosong */
 /* FS : Jika List tidak kosong, semua info yang disimpan pada elemen list */
 /*	diprint. Jika list kosong, hanya menuliskan "List Kosong" */
 
-void DeAlokasiGroup (address P);
+void DeAlokasiGroup (addressGroup P);
 /* IS : P terdefinisi */
 /* FS : P dikembalikan ke sistem */
 /* Melakukan dealokasi / pengembalian addressGroup P ke system */
 
-void DeAlokasiTim (addressTim P);
+void DeAlokasiTeam (addressTeam P);
 /* IS : P terdefinisi */
 /* FS : P dikembalikan ke sistem */
-/* Melakukan dealokasi / pengembalian addressTim P ke system */
+/* Melakukan dealokasi / pengembalian addressTeam P ke system */
 
 void DelPGroup (ListGroup * L, infotype X);
 /* IS : L sembarang */
@@ -101,11 +93,11 @@ void DelPGroup (ListGroup * L, infotype X);
 /* Jika tidak ada elemen listGroup dengan Info(P) = X, maka listGroup tetap */
 /* listGroup mungkin menjadi kosong karena penghapusan */
 
-void DelPTim (ListTim * L, infotype X);
+void DelPTeam (ListTeam * L, infotype X);
 /* IS : L sembarang */
-/* FS : Jika ada elemen listTim beraddressMhs P, dengan Info(P) = X */
-/* 	Maka P dihapus dari listTim dan di dealokasi */
-/* Jika tidak ada elemen listTim dengan Info(P) = X, maka listTim tetap */
-/* listTim mungkin menjadi kosong karena penghapusan */
+/* FS : Jika ada elemen ListTeam beraddressMhs P, dengan Info(P) = X */
+/* 	Maka P dihapus dari ListTeam dan di dealokasi */
+/* Jika tidak ada elemen ListTeam dengan Info(P) = X, maka ListTeam tetap */
+/* ListTeam mungkin menjadi kosong karena penghapusan */
 
 #endif
