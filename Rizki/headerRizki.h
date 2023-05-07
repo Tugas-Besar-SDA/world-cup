8fdf2f6a0ccbd6272b97ea862841470f8a1adc52
RizkiGunawan09 <123919208+RizkiGunawan09@users.noreply.github.com>
2023-05-07 19:22:38 +0700

    ngerapihin header tapi belum ada penjelasan modul

--- Rizki/headerRizki.h
+++ Rizki/headerRizki.h
@@ -5,13 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
+#include <time.h>
 
 typedef char* infotype;
 typedef struct tNode *addressNode;
 typedef struct tNode {
     addressNode left;
+    int alamat, gol, golPenalty;
     infotype value;
-    int gol; // nilai default
     addressNode right;
 }Node;
 
@@ -20,22 +22,50 @@ typedef struct tReferee{
 	addressNode rightTeam;
 }Referee;
 
-int tree_height(int n);
-
-addressNode create_node(int value);
+/* Pembuatan tree */
+addressNode create_node(int number);
 
 addressNode build_tree(int arr[], int start, int end);
 
+int tree_height(int n);
+
+void inputTim(addressNode root, infotype X, int maxNode);
+
+addressNode searchNode(addressNode root, int key);
+
+bool isRoot(addressNode root);
+
+void upgradeLeftTeam(Referee pointerPertandingan, addressNode nodeSearch);
+
+void upgradeRightTeam(Referee pointerPertandingan, addressNode nodeSearch);
+
+void babakPenalty(addressNode root, Referee pointerPertandingan);
+
+void upgradeFinalTeam(addressNode root, Referee pointerPertandingan);
+
+int calculateLevelTree(addressNode root);
+
+/* Proses pertandingan */
+void babakGugur(addressNode root, int maxNode);
+
+void babakPenalty(addressNode root, Referee pointerPertandingan);
+
+void upgradeLeftTeam(Referee pointerPertandingan, addressNode nodeSearch);
+
+void upgradeRightTeam(Referee pointerPertandingan, addressNode nodeSearch);
+
+/* Modul Support */
+addressNode searchNode(addressNode root, int key);
+
+int randomGol();
+
+int randomPenalty();
+
+/* Modul Traversal */
 void preorder_traversal(addressNode root);
 
 void inorder_traversal(addressNode root);
 
 void postorder_traversal(addressNode root);
-
-addressNode refereeSearchTeam(addressNode referee);
-
-int jumlahDaun(int maxNode);
-
-//void groupToTree()
 
 #endif
