4bce90fb721dbe6039522bae8d0b2c5780dd63f1
adi94958 <117360640+adi94958@users.noreply.github.com>
2023-05-10 14:39:31 +0700

    tambah modul, ubah parameter

--- Adi/mainAdi.c
+++ Adi/mainAdi.c
@@ -4,44 +4,42 @@
 int main(){
 	//kamus data
 	addressGroup HeadGroup, Group;
-	addressTeam Team;
+	addressTeam HeadTeam, Team;
 	ListGroup MyListGroup;
-	ListTeam MyListTim;
-//	int JumlahGroup = 1;
-	int pilihMenu, pilihMenuDelete;
-	CreateListGroup(&MyListGroup);
+	ListTeam MyListTeam;
+	int pilihMenu;
 	
 	//program
-	for(;;){
+	CreateListGroup(&MyListGroup);
+	ReadTeam(&MyListGroup);
+    	for(;;){
 		system("cls");
 		if(!isEmptyGroup(MyListGroup)){
-			PrintInfo(MyListGroup);
+			PrintInfo (MyListGroup);
 		}
-		printf("Pilih opsi\n1. Tambah grup\n2. Tambah tim\n3. Delete grup\n4. Delete tim\n5. Pertandingan Group \n0. Exit\nMasukkan pilihan Anda: ");
-	    	scanf("%d", &pilihMenu);
-		switch(pilihMenu) {
+		
+		printf("Pilih opsi\n1. Tambah grup\n2. Tambah tim\n3. Delete grup\n4. Delete tim\n5. Tentang tim\n0. Exit\nMasukkan pilihan Anda: ");
+	    scanf("%d", &pilihMenu);
+	    switch(pilihMenu) {
 	    	case 1:
-	    		addGroup(&MyListGroup, &HeadGroup);
-	    	break;
+	    		MenuAddGroup(&MyListGroup);
+			break;
 	    	case 2:
-	    		addTeam(&MyListGroup, &MyListTim, &HeadGroup);
+	    		MenuAddTeam(&MyListGroup);
 	    	break;
 	    	case 3:
-	    		DelGroup(&MyListGroup, &HeadGroup);
-			break;
-			case 4:
-			    	DelTeam(&MyListGroup, &MyListTim, &HeadGroup);
-			break;
-//			case 5:
-//			    	Group = First(MyListGroup);
-//			    	GroupStage(Group);
-//			    	Group = nextGroup(Group);
-//			    	system("pause");
-			break;
-		    case 0:
-		    	exit(0);
+	    		MenuDelGroup(&MyListGroup);
 		    break;
-		    default:
+		    case 4:
+		    	MenuDelTeam(&MyListGroup);
+		    break;
+		    case 5:
+		    	AboutTeam();
+		    break;
+	    	case 0:
+	    		exit(0);
+	    	break;
+	    	default:
 	    		printf("Kesalahan input! Menu %d tidak tersedia \n", pilihMenu);
 	    		system("pause");
 	    	break;
@@ -51,7 +49,7 @@ int main(){
 	//finishing
 	Group = First(MyListGroup);
 	DeAlokasiGroup (Group);
-	Team = First(MyListTim);
+	Team = First(MyListTeam);
 	DeAlokasiTeam (Team);
 	return 0;
 }
